7ae5285a5df78f15feed1b8c1cb290896d6b13a0
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-06-06 10:01:30 +0200

    add conditional rendering in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import './App.css';
 import QuestionCard from './components/QuestionCard';
-import { fetchQuizQuestions, Difficulty } from './API';
+import { fetchQuizQuestions, Difficulty, QuestionState } from './API';
 import { useState } from 'react';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-  const [loading, stLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswer, setUserAnswer] = useState([]);
+  const [userAnswer, setUserAnswer] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+  const [error, setError] = useState(false);
 
-  const startQuiz = async () => {};
+  console.log(questions);
+
+  const startQuiz = async () => {
+    try {
+      setLoading(true);
+      setGameOver(false);
+      const newQuestions = await fetchQuizQuestions(
+        TOTAL_QUESTIONS,
+        Difficulty.EASY
+      );
+
+      setQuestions(newQuestions);
+      setScore(0);
+      setUserAnswer([]);
+      setNumber(0);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
   const nextQuestion = () => {};
 
@@ -23,31 +52,32 @@ const App = () => {
         <h1>Quiz TypeScript</h1>
       </header>
       <main>
-        <button className="start-btn" onClick={startQuiz}>
-          Start Quiz
-        </button>
-        <p className="score">Score:</p>
-        <p className="loading">Loading Questions ...</p>
-        {/* <QuestionCard
-          questionNo={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswer ? userAnswer[number] : undefined}
-          callback={checkAnswer}
-        /> */}
-        <button className="next-btn" onClick={nextQuestion}>
-          Next Question
-        </button>
-        <button
-          onClick={() =>
-            fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY).then((res) => {
-              console.log(res);
-            })
-          }
-        >
-          click
-        </button>
+        {gameOver || userAnswer.length === TOTAL_QUESTIONS ? (
+          <button className="start-btn" onClick={startQuiz}>
+            Start Quiz
+          </button>
+        ) : null}
+        {!gameOver && <p className="score">Score:</p>}
+        {loading && <p className="loading">Loading Questions ...</p>}
+        {error && 'Ups... something went wrong!'}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNo={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswer ? userAnswer[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver &&
+        !loading &&
+        userAnswer.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next-btn" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
       </main>
     </div>
   );
